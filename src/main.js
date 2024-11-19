3759f7aba7fc9a93d240d99ffb237f6532fb37ec
Erik Westlund <erik.westlund@student.swic.edu>
2024-11-18 20:33:33 -0600

    Following class

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-const paragraphs = document.querySelectorAll("p");
+// const paragraphs = document.querySelectorAll("p");
 
-console.log(Array.from(paragraphs).map((p) => p.innerText));
+// console.log(Array.from(paragraphs).map((p) => p.innerText));
 
-const ps = Array.from(paragraphs);
+// const ps = Array.from(paragraphs);
 
-console.log(ps.map((p) => p.innerText));
+// console.log(ps.map((p) => p.innerText));
+
+// const heading = document.createElement("h1");
+// const headingText = document.createTextNode("Big Head");
+// heading.appendChild(headingText);
+// document.body.appendChild(heading);
+
+// document.body.appendChild(document.createElement("h2")).textContent =
+//   "Hello World";
+// One way to do it but it's too much code
+
+import H2 from "./components/h2";
+
+const app = document.getElementById("app");
+
+app.innerHTML = H2("Hello World!");
